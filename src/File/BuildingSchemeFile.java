fa5098b2eb7f5798268b8f39054ef5850ed95610
Felix Manuel <felixmanuel88@gmail.com>
2017-07-31 22:44:36 +0200

    BuildingSchemeFile
    Class wich read a .json file and convert into a java object

--- src/File/BuildingSchemeFile.java
+++ src/File/BuildingSchemeFile.java
@@ -1,7 +1,13 @@
 package File;
 
-import Building_Scheme.Building_Scheme;
+import Building_Scheme.BuildingScheme;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author Felix Manuel Mellado
@@ -9,9 +15,23 @@ import com.google.gson.JsonParser;
 public class BuildingSchemeFile {
     //Attributes
     private static final String FOLDER = "buildingScheme//";
-    private Building_Scheme building;
+    private BuildingScheme building;
     
+    //Constuctor
     public BuildingSchemeFile(String name){
         JsonParser parser = new JsonParser();
+        Gson gson = new Gson();
+        try {
+            FileReader fr = new FileReader(FOLDER + name);            
+            JsonElement datos = parser.parse(fr);
+            this.building = gson.fromJson(datos.toString(), BuildingScheme.class);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(BuildingSchemeFile.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    //Getter Methods
+    public BuildingScheme getBuilding(){
+        return this.building;
     }
 }
