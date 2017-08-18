16f71351edadb63a26a4e128dd601cb9297bed33
Felix Manuel <felixmanuel88@gmail.com>
2017-08-18 23:12:24 +0200

    AgentScheme
    I have done this task:
    - Create the AgentScheme to have the agent´s position on the plane.

---
+++ src/Agent_Scheme/AgentScheme.java
@@ -0,0 +1,18 @@
+package Agent_Scheme;
+
+import Utilities.Coordinate;
+
+/**
+ * @author Felix Manuel Mellado
+ */
+public class AgentScheme {
+    //Attributes
+    private Coordinate coordinate;
+    private int identification;
+    
+    //Constructor
+    public AgentScheme(){
+        this.coordinate = new Coordinate();
+        this.identification = 0;
+    }
+}
